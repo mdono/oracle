3e7e1d685eccb2d62fc4bbf610dff69b346d212b
mdono <mdono@intecap.edu.gt>
2022-08-13 07:49:10 -0600

    ejemplo

--- cursosabado1938.sql
+++ cursosabado1938.sql
@@ -38,3 +38,63 @@ CREATE TABLE departamento(
 
 SELECT * FROM departamento;
 DESCRIBE departamento;
+
+INSERT INTO departamento(depto_id, nombre, localidad) VALUES(
+    0101,
+    'Guatemala',
+    'Guatemala'
+);
+
+SELECT * FROM departamento;
+DROP TABLE departamento;
+
+CREATE TABLE departamento(
+    depto_id NUMBER(9) PRIMARY KEY,
+    nombre VARCHAR2(100),
+    localidad VARCHAR2(300),
+    fecha_creacion DATE DEFAULT SYSDATE,
+    creacion TIMESTAMP DEFAULT SYSDATE,
+    estado CHAR(1)
+);
+
+SELECT * FROM departamento;
+
+INSERT INTO departamento(depto_id, nombre, localidad, estado) VALUES(
+    0101,
+    'Guatemala',
+    'Guatemala',
+    'a'
+);
+
+CREATE TABLE date_tab (
+   ts_col      TIMESTAMP,
+   tsltz_col   TIMESTAMP WITH LOCAL TIME ZONE,
+   tstz_col    TIMESTAMP WITH TIME ZONE);
+
+
+ALTER SESSION SET TIME_ZONE = '-8:00';
+
+INSERT INTO date_tab VALUES (  
+   TIMESTAMP'1999-12-01 10:00:00',
+   TIMESTAMP'1999-12-01 10:00:00',
+   TIMESTAMP'1999-12-01 10:00:00');
+   
+INSERT INTO date_tab VALUES (
+   TIMESTAMP'1999-12-02 10:00:00 -8:00', 
+   TIMESTAMP'1999-12-02 10:00:00 -8:00',
+   TIMESTAMP'1999-12-02 10:00:00 -8:00');
+
+SELECT TO_CHAR(ts_col, 'DD-MON-YYYY HH24:MI:SSxFF') AS ts_date,
+   TO_CHAR(tstz_col, 'DD-MON-YYYY HH24:MI:SSxFF TZH:TZM') AS tstz_date
+   FROM date_tab
+   ORDER BY ts_date, tstz_date;
+
+SELECT SESSIONTIMEZONE, 
+   TO_CHAR(tsltz_col, 'DD-MON-YYYY HH24:MI:SSxFF') AS tsltz
+   FROM date_tab
+   ORDER BY sessiontimezone, tsltz;
+   
+-- 4 palabras reservadas muy importantes y son:
+-- 1. CREATE 2. ALTER 3. DROP // 2. SELECT 
+
+
